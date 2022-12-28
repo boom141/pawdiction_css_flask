da93db1badd824bad926be3e6f12fb72544df979
Nijiiro <61643144+boom141@users.noreply.github.com>
2022-12-29 01:02:47 +0800

    Add files via upload

---
+++ PawDiction/main.py
@@ -0,0 +1,116 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from PIL import Image
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+
+ALLOWED_EXTENSIONS = set(['jpg', 'png'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def read_image():
+    image = request.files['file']
+    filename = secure_filename(image.filename)
+    if allowed_file(filename):
+        image_path = "./images/" + filename
+        image.save(image_path)
+
+        return image_path
+    else:
+        return False
+
+def load_model(model_path):
+    model = tf.keras.models.load_model(model_path,custom_objects={"KerasLayer": hub.KerasLayer})
+    
+    return model
+
+def process_image(image_path, image_size=224):
+    """
+    Takes an image file path and turns the image into a Tensor.
+    """
+    # Read in an image file
+    image = tf.io.read_file(image_path)
+
+    # Turn the jpeg image into numerical Tensor with 3 color channels
+    image = tf.image.decode_jpeg(image, channels=3)
+
+    # Convert the color channel values from 0-255 to 0-1 vales
+    image = tf.image.convert_image_dtype(image, tf.float32)
+
+    # Resize the image to our desired value (244, 244)
+    image = tf.image.resize(image, size=(image_size, image_size))
+    
+    return image
+
+def imageType_convert(image):
+    im1 = Image.open(image)
+    new_path_image = f'./images/target.jpg'
+    im1.save(new_path_image)
+
+    return new_path_image
+
+class PredictEmotion(Resource):
+    def post(self):
+        # Read image
+        image = read_image()
+        converted_image = imageType_convert(image)
+
+        if converted_image:
+            # Process image
+            processed_image = process_image(converted_image, 224)
+            processed_image = converted_image = np.expand_dims(processed_image, axis=0)
+            
+            # Load model
+            model = load_model("saved_model.h5")
+
+            # Predict the emotion of model
+            prediction = np.round(model.predict(converted_image), 3)*100
+
+
+
+            # Return response as list.
+            return prediction[0].tolist()
+        else:
+            return "Please upload a valid image"
+
+class isADog(Resource):
+    def post(self):
+        # Read image
+        image = read_image()
+        converted_image = imageType_convert(image)
+
+        if converted_image:
+            # Process image
+            processed_image = process_image(converted_image, 256)
+            processed_image = converted_image = np.expand_dims(processed_image, axis=0)
+            
+            # Load model
+            model = load_model("Dogs-vs-Cats_model.h5")
+
+            # Predict the emotion of model
+            prediction = model.predict(converted_image)
+
+            # Return response as list.
+            return prediction[0].tolist()
+        else:
+            return "Please upload a valid image"
+
+api.add_resource(PredictEmotion, "/predict_emotion")
+api.add_resource(isADog, "/is_a_dog")
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- PawDiction/request-data-upload.js
+++ PawDiction/request-data-upload.js
@@ -1,48 +1,64 @@
 
-let dog_validated = false
- document.getElementById("upload").addEventListener("change", event => {
-    event.preventDefault()
-    const uploaded_image = event.target.files[0]
+const upload_btn =  document.getElementById("upload")
+ upload_btn.onchange = (e) =>{
+    e.preventDefault()
+    const input_image = upload_btn.files[0]
     const image_data = new FormData()
-    image_data.append('file', uploaded_image)
+    image_data.append('file', input_image)
 
     const reader = new FileReader();
-    reader.addEventListener("load", () => {
-    document.querySelector("#canvas").style.backgroundImage = `url(${reader.result})`;
-    axios.post('http://pawdiction.pythonanywhere.com/is_a_dog',image_data)
+    reader.onload = () =>{
+        document.querySelector("#canvas").style.backgroundImage = `url(${reader.result})`;
+    }
+    reader.readAsDataURL(input_image);
+
+    axios.post('http://127.0.0.1:5000/is_a_dog',image_data)
     .then(res => {
         if(Array.isArray(res.data)){
-            dog_validated = true
             console.log('verifying complete')
+            $('#predict').css('pointer-events', 'all')
         }else{
             console.log('image is not a dog')
-    }})
-    .catch( error => {console.log(error)})
+    }}).catch( error => {console.log(error)}) 
 
-    });
-    reader.readAsDataURL(uploaded_image);
-    
-    document.getElementById('predict').addEventListener('click', event =>{
+
+document.getElementById('predict').addEventListener('click', event =>{
     event.preventDefault()
     result_transition()
-    if(dog_validated){
-        axios.post('http://pawdiction.pythonanywhere.com/predict_emotion',image_data)
-        .then(res =>{
-            $('.loading-wrapper').fadeOut('slow')
-            console.log(res.data)
-            
-        })
-        .catch(error =>{console.log(error)})
-    }else{
-        console.log('image is not a dog')
-    }
+
+    axios.post('http://127.0.0.1:5000/predict_emotion', image_data)
+    .then(res =>{
+        const absolute_results = []
+        $('.loading-wrapper').css('display', 'none')
+        $('.result-section').css('display', 'flex')
+        for(let result of res.data){
+            absolute_results.push(Math.floor(result))
+        }
+        load_meter(absolute_results)
+        
+    })
+    .catch(error =>{console.log(error)})
 
 })
 
-})
+}
+
 
 const result_transition = () =>{
     $('.form').css('display', 'none')
     $('.options').css('display', 'none')
     $('.loading-wrapper').css('display', 'flex')
+}
+
+const meter = document.querySelectorAll('.actual-meter')
+
+const emotion = ['ANGRY', 'HAPPY', 'RELAXED', 'SAD']
+const load_meter = (results) =>{
+    meter.forEach((element,index) =>{
+        $(element).animate({width: `${results[index]}%`})
+    })
+    
+    const highest_percentage_index = results.indexOf(Math.max(...results))
+    $('.result-section').append(`<h1 class="result-text">Final result: ${emotion[highest_percentage_index]} (${results[highest_percentage_index]}%)</h1>`)
+
 }
